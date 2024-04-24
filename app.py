5e7eadd7b8b1e1ab31ee0c3d8c6142d4e58e182d
Smital-Lunawat <lunawatsmital@gmail.com>
2024-04-24 04:34:32 -0500

    hosting added

--- app.py
+++ app.py
@@ -117,5 +117,5 @@ def ask_question():
     return jsonify({'answer': answer})
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0")     
-    app.run(debug=True)
+    app.run(host="0.0.0.0")     
+    # app.run(debug=True)
